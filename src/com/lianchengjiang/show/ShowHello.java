474d0b346e75b827de52ed3bffec0a71e26895c2
DoubStar <jlc_1981@qq.com>
2016-12-29 13:52:33 +0800

    create

---
+++ src/com/lianchengjiang/show/ShowHello.java
@@ -0,0 +1,13 @@
+package com.lianchengjiang.show;
+
+public class ShowHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+	}
+
+}
